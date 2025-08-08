cf00b37bfb447fe63b2cc72a3f2376c69e41fa2a
s1n7ax <srineshnisala@gmail.com>
2025-08-08 14:41:28 +0530

    chore

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -6,11 +6,11 @@ vim.pack.add({
 	'https://github.com/mfussenegger/nvim-dap',
 	'https://github.com/JavaHello/spring-boot.nvim',
 	{ src = 'https://github.com/nvim-java/lua-async-await', 		version = os.getenv('LUA_ASYNC_AWAIT_BRANCH') },
-	{ src = 'https://github.com/nvim-java/nvim-java', 					version = os.getenv('NVIM_JAVA_BRANCH') },
 	{ src = 'https://github.com/nvim-java/nvim-java-core', 			version = os.getenv('NVIM_JAVA_CORE_BRANCH') },
 	{ src = 'https://github.com/nvim-java/nvim-java-dap', 			version = os.getenv('NVIM_JAVA_DAP_BRANCH') },
 	{ src = 'https://github.com/nvim-java/nvim-java-refactor', 	version = os.getenv('NVIM_JAVA_REFACTOR_BRANCH') },
 	{ src = 'https://github.com/nvim-java/nvim-java-test', 			version = os.getenv('NVIM_JAVA_TEST_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java', 					version = os.getenv('NVIM_JAVA_BRANCH') },
 })
 
 require('mason').setup()
