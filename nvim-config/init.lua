8f67257a38cb2fcbb30995cb818fe6e11ce2f378
s1n7ax <srineshnisala@gmail.com>
2025-08-08 14:45:42 +0530

    chore

--- nvim-config/init.lua
+++ nvim-config/init.lua
@@ -1,16 +1,21 @@
 -- stylua: ignore
+local function get_branch(env_var)
+	local branch = os.getenv(env_var)
+	return (branch and branch ~= '') and branch or nil
+end
+
 vim.pack.add({
 	'https://github.com/mason-org/mason.nvim',
 	'https://github.com/MunifTanjim/nui.nvim',
 	'https://github.com/neovim/nvim-lspconfig',
 	'https://github.com/mfussenegger/nvim-dap',
 	'https://github.com/JavaHello/spring-boot.nvim',
-	{ src = 'https://github.com/nvim-java/lua-async-await', 		version = os.getenv('LUA_ASYNC_AWAIT_BRANCH') },
-	{ src = 'https://github.com/nvim-java/nvim-java-core', 			version = os.getenv('NVIM_JAVA_CORE_BRANCH') },
-	{ src = 'https://github.com/nvim-java/nvim-java-dap', 			version = os.getenv('NVIM_JAVA_DAP_BRANCH') },
-	{ src = 'https://github.com/nvim-java/nvim-java-refactor', 	version = os.getenv('NVIM_JAVA_REFACTOR_BRANCH') },
-	{ src = 'https://github.com/nvim-java/nvim-java-test', 			version = os.getenv('NVIM_JAVA_TEST_BRANCH') },
-	{ src = 'https://github.com/nvim-java/nvim-java', 					version = os.getenv('NVIM_JAVA_BRANCH') },
+	{ src = 'https://github.com/nvim-java/lua-async-await', 		version = get_branch('LUA_ASYNC_AWAIT_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-core', 			version = get_branch('NVIM_JAVA_CORE_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-dap', 			version = get_branch('NVIM_JAVA_DAP_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-refactor', 	version = get_branch('NVIM_JAVA_REFACTOR_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-test', 			version = get_branch('NVIM_JAVA_TEST_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java', 					version = get_branch('NVIM_JAVA_BRANCH') },
 })
 
 require('mason').setup()
