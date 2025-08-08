e99542d8f7cc84953e8d13a3f0bf840b4571f8db
s1n7ax <srineshnisala@gmail.com>
2025-08-08 14:09:25 +0530

    chore

---
+++ nvim-config/init.lua
@@ -0,0 +1,29 @@
+-- stylua: ignore
+vim.pack.add({
+	{ src = 'https://github.com/nvim-java/nvim-java-test', version = os.getenv('NVIM_JAVA_TEST_BRANCH') },
+	{ src = 'https://github.com/mason-org/mason.nvim', version = os.getenv('MASON_NVIM_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-core', version = os.getenv('NVIM_JAVA_CORE_BRANCH') },
+	{ src = 'https://github.com/nvim-java/lua-async-await', version = os.getenv('LUA_ASYNC_AWAIT_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-refactor', version = os.getenv('NVIM_JAVA_REFACTOR_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java-dap', version = os.getenv('NVIM_JAVA_DAP_BRANCH') },
+	{ src = 'https://github.com/MunifTanjim/nui.nvim', version = os.getenv('NUI_NVIM_BRANCH') },
+	{ src = 'https://github.com/neovim/nvim-lspconfig', version = os.getenv('NVIM_LSPCONFIG_BRANCH') },
+	{ src = 'https://github.com/mfussenegger/nvim-dap', version = os.getenv('NVIM_DAP_BRANCH') },
+	{ src = 'https://github.com/JavaHello/spring-boot.nvim', version = os.getenv('SPRING_BOOT_NVIM_BRANCH') },
+	{ src = 'https://github.com/nvim-java/nvim-java', version = os.getenv('NVIM_JAVA_BRANCH') },
+})
+
+require('mason').setup()
+require('java').setup({})
+require('lspconfig').jdtls.setup({})
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	callback = function(ev)
+		-- stylua: ignore
+		vim.lsp.completion.enable( true, ev.data.client_id, ev.buf, { autotrigger = true })
+	end,
+})
+
+vim.opt.completeopt = { 'menuone', 'noselect', 'popup' }
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
